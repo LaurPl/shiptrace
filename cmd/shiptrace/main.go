fc420063a144a34a1f5c53923ac8bc41555eb8a7
Laurentiu Emanuel Placinta <laurentiu.placinta@proton.me>
2026-05-14 18:25:30 +0300

    chore: embed web/dist placeholder so //go:embed compiles before bundle build

--- cmd/shiptrace/main.go
+++ cmd/shiptrace/main.go
@@ -1,13 +1,18 @@
 package main
 
 import (
+	"embed"
 	"fmt"
 	"os"
 
 	"github.com/LaurPl/shiptrace/internal/cli"
 )
 
+//go:embed all:web/dist
+var bundleFS embed.FS
+
 func main() {
+	cli.SetBundle(bundleFS)
 	if err := cli.Execute(); err != nil {
 		fmt.Fprintln(os.Stderr, "error:", err)
 		os.Exit(1)
